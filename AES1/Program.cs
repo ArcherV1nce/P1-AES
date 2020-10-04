8f96ec5c46a6852704c29e417d181f99bd3fbf81
Aron-le-V1nce <ArcherV1nce@users.noreply.github.com>
2020-10-05 01:41:19 +0300

    Initial commit

---
+++ AES1/Program.cs
@@ -0,0 +1,180 @@
+﻿using System;
+using static System.Console;
+using System.Diagnostics;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Windows;
+using System.Security.Cryptography;
+
+namespace AES1
+{
+    class Program
+    {
+        private AES128 aES128 = new AES128();
+        static void Main(string[] args)
+        {
+            AES128 aES = new AES128();
+            aES.GetFileDir();
+            aES.AES_Encrypt("D:\\4KNU\\EnctTest\\Test\\1551550075_6e5750a39125034c.gif", "123");
+            aES.AES_Decrypt("D:\\4KNU\\EnctTest\\Test\\1551550075_6e5750a39125034c.gif.aes", "123");
+        }
+    }
+
+
+
+    class AES128
+    {
+        private string fileDirectory;
+
+        public void GetFileDir()
+        {
+            fileDirectory = Console.ReadLine();
+        }
+
+        public void AES_Encrypt(string inputFile, string password)
+        {
+
+            inputFile = "D:\\4KNU\\EnctTest\\Test\\1551550075_6e5750a39125034c.gif";
+
+            
+            byte[] salt = GenerateRandomSalt();
+
+            
+            FileStream fsCrypt = new FileStream(inputFile + ".aes", FileMode.Create);
+
+            
+            byte[] passwordBytes = System.Text.Encoding.UTF8.GetBytes(password);
+
+            //Set Rijndael symmetric encryption algorithm
+            RijndaelManaged AES = new RijndaelManaged();
+            AES.KeySize = 256;
+            AES.BlockSize = 128;
+            AES.Padding = PaddingMode.PKCS7;
+
+            //"What it does is repeatedly hash the user password along with the salt." High iteration counts.
+            var key = new Rfc2898DeriveBytes(passwordBytes, salt, 50000);
+            AES.Key = key.GetBytes(AES.KeySize / 8);
+            AES.IV = key.GetBytes(AES.BlockSize / 8);
+
+            AES.Mode = CipherMode.CFB;
+
+            //write salt to the begining of the output file, so in this case can be random every time
+            fsCrypt.Write(salt, 0, salt.Length);
+
+            CryptoStream cs = new CryptoStream(fsCrypt, AES.CreateEncryptor(), CryptoStreamMode.Write);
+
+            FileStream fsIn = new FileStream(inputFile, FileMode.Open);
+
+            //create a buffer (1mb) so only this amount will allocate in the memory and not the whole file
+            byte[] buffer = new byte[1048576];
+            int read;
+
+            try
+            {
+                while ((read = fsIn.Read(buffer, 0, buffer.Length)) > 0)
+                {
+                    cs.Write(buffer, 0, read);
+                }
+
+                //close up
+                fsIn.Close();
+
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine("Error: " + ex.Message);
+            }
+            finally
+            {
+                cs.Close();
+                fsCrypt.Close();
+            }
+        }
+
+        public void AES_Decrypt(string inputFile, string password)
+        {
+            //todo:
+            // - create error message on wrong password
+            // - on cancel: close and delete file
+            // - on wrong password: close and delete file!
+            // - create a better filen name
+            // - could be check md5 hash on the files but it make this slow
+
+            inputFile = "D:\\4KNU\\EnctTest\\Test\\1551550075_6e5750a39125034c.gif.aes";
+
+            byte[] passwordBytes = System.Text.Encoding.UTF8.GetBytes(password);
+            byte[] salt = new byte[32];
+
+            FileStream fsCrypt = new FileStream(inputFile, FileMode.Open);
+            fsCrypt.Read(salt, 0, salt.Length);
+
+            RijndaelManaged AES = new RijndaelManaged();
+            AES.KeySize = 256;
+            AES.BlockSize = 128;
+            var key = new Rfc2898DeriveBytes(passwordBytes, salt, 50000);
+            AES.Key = key.GetBytes(AES.KeySize / 8);
+            AES.IV = key.GetBytes(AES.BlockSize / 8);
+            AES.Padding = PaddingMode.PKCS7;
+            AES.Mode = CipherMode.CFB;
+
+            CryptoStream cs = new CryptoStream(fsCrypt, AES.CreateDecryptor(), CryptoStreamMode.Read);
+
+            FileStream fsOut = new FileStream(inputFile + ".decrypted", FileMode.Create);
+
+            int read;
+            byte[] buffer = new byte[1048576];
+
+            try
+            {
+                while ((read = cs.Read(buffer, 0, buffer.Length)) > 0)
+                {
+                    fsOut.Write(buffer, 0, read);
+                }
+            }
+            catch (System.Security.Cryptography.CryptographicException ex_CryptographicException)
+            {
+                Debug.WriteLine("CryptographicException error: " + ex_CryptographicException.Message);
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine("Error: " + ex.Message);
+            }
+
+            try
+            {
+                cs.Close();
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine("Error by closing CryptoStream: " + ex.Message);
+            }
+            finally
+            {
+                fsOut.Close();
+                fsCrypt.Close();
+            }
+        }
+
+        public static byte[] GenerateRandomSalt()
+        {
+            byte[] data = new byte[32];
+
+            using (RNGCryptoServiceProvider rng = new RNGCryptoServiceProvider())
+            {
+                // Ten iterations.
+                for (int i = 0; i < 10; i++)
+                {
+                    // Fill buffer.
+                    rng.GetBytes(data);
+                }
+            }
+            return data;
+        }
+
+
+    }
+    
+}
